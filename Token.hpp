7a31fea78580c5a61a1fc162e063c174b275b612
vicente <vicentesalvaradog@gmail.com>
2024-07-25 13:45:46 +0200

    Rules class added with basic rules to try while lexing

---
+++ Token.hpp
@@ -0,0 +1,30 @@
+
+#ifndef TOKEN_HPP
+#define TOKEN_HPP
+#include <string>
+
+namespace JsonParser
+{
+	enum class TokenType
+	{
+		LEFT_BRACE,	   // {
+		RIGHT_BRACE,   // }
+		LEFT_BRACKET,  // [
+		RIGHT_BRACKET, // ]
+		COLON,		   // :
+		COMMA,		   // ,
+		STRING,		   // "..."
+		NUMBER,		   // 123, -123, 1.23, etc.
+		BOOLEAN,	   // true, false
+		NUL,		   // null
+		END,		   // End of input
+		UNDEFINED,	   // Ini of var
+	}; // enum class TokenType
+
+	struct Token
+	{
+		TokenType Type;
+		std::string Value;
+	}; // struct Token
+}
+#endif
